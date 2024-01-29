a3891f41ab28bce80c2d0778f84e535b74bb86ca
Jacek Bastin <jacek.bastin@gmail.com>
2024-01-29 19:59:38 +0000

    Added form on homepage and post url to get data

--- app.rb
+++ app.rb
@@ -5,11 +5,22 @@ require "pry-byebug"
 require_relative 'app/repositories/repository/'
 require_relative 'app/models/task'
 
+csv_file = File.join(__dir__, 'data/tasks.csv')
+repository = Repository.new(csv_file)
+
 get "/" do
-    #Create the repository
-    csv_file = File.join(__dir__, 'data/tasks.csv')
-    repository = Repository.new(csv_file)
     #List_all tasks from the Repository
     @tasks = repository.all
   erb :'/index'
+end
+
+post "/add_task" do
+  name = params[:name]
+  # Create new instance of Task
+  task = Task.new(name)
+
+  #Save to repo
+  repository.add(task)
+  redirect "/"
+
 end
